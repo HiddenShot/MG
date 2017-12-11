6c47133fd8c8e90c2c188442e7d36c791c0d8e0c
fzkb13 <12990548+fzkb13@users.noreply.github.com>
2017-12-10 23:08:06 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir arduino_payloads
+mkdir digispark_payloads
+mkdir kb04rd_payloads
+git clone https://github.com/hak5darren/USB-Rubber-Ducky.git
+sudo chmod +x mg.py
+echo -e '\e[1;31mNow put ./mg.py on the terminal and enjoy!\e[1m'
+echo -e '\e[1;34mWith love: ~HS~\e[1m'
